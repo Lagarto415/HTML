6d65cbf56711eb71a242b1e4743a1ef460266ac1
Lagarto415 <126022704+Lagarto415@users.noreply.github.com>
2024-01-04 14:17:56 +0000

    crypt

---
+++ js/crypt.js
@@ -0,0 +1,39 @@
+const moveobj = document.getElementById('move');
+const wordobj = document.getElementById('word');
+const finalobj = document.getElementById('final');
+
+function calc (){
+    raw = wordobj.value;
+    const mov = parseInt(moveobj.value);
+    var x = 0;
+    var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var fullAlphabet = alphabet + alphabet + alphabet;
+    let fin = "";
+    var temp = '';
+
+    while (x<raw.length){
+        temp = raw.charCodeAt(x) - 65
+        console.log(temp+mov)
+        fin += fullAlphabet.charAt(temp+mov);
+        x++;
+    }
+    finalobj.innerHTML = fin;
+}
+
+function decode(){
+    raw = wordobj.value;
+    const mov = parseInt(moveobj.value);
+    var x = 0;
+    var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var fullAlphabet = alphabet + alphabet + alphabet;
+    let fin = "";
+    var temp = '';
+
+    while (x<raw.length){
+        temp = raw.charCodeAt(x) - 65
+        console.log(temp+mov)
+        fin += fullAlphabet.charAt(temp-mov+26);
+        x++;
+    }
+    finalobj.innerHTML = fin;
+}
